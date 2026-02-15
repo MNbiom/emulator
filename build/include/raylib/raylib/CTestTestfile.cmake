6027a917a9703cdbb8d66f5d4c10e0b35edfd80f
MNbiom <zientak.tomek@gmail.com>
2026-02-15 18:29:04 +0100

    added gitignore, removed build and added include

--- build/include/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tomek/Desktop/VSC2/c++/emulator/include/raylib/src
-# Build directory: C:/Users/tomek/Desktop/VSC2/c++/emulator/build/include/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
